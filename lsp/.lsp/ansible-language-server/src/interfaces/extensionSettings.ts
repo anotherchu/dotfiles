1e5f369c7ad920688a695bf2e2f66e4fb493b0cd
Thiago Chu <thiagomei@id.uff.br>
2021-08-13 01:47:23 -0300

    herpderp (redux)

--- lsp/.lsp/ansible-language-server/src/interfaces/extensionSettings.ts
+++
@@ -1,5 +0,0 @@
-export interface ExtensionSettings {
-  ansible: { path: string; useFullyQualifiedCollectionNames: boolean };
-  ansibleLint: { enabled: boolean; path: string; arguments: string };
-  python: { interpreterPath: string; activationScript: string };
-}
