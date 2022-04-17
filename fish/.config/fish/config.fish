008c198a7f568a482f3dae1b4c7fa89156b98a21
Thiago Chu <thiagomei@id.uff.br>
2022-04-17 16:48:26 -0300

    pep8 - cause I fell like doing a barrel roll

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,3 +30,5 @@ status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 status is-interactive; and pyenv virtualenv-init - | source
 # end pyenv configuration
+
+fish_ssh_agent
