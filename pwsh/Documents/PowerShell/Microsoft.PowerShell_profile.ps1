7f3cfe71fa62478d9a818983ff02d5d981f74c1c
Thiago Chu <thiagomei@id.uff.br>
2023-07-15 16:21:44 -0300

    well crap.

--- pwsh/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ pwsh/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,18 +2,8 @@ Import-Module PSReadLine
 
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\chu.omp.json | Invoke-Expression
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
-
 Set-Alias -Name n -Value nvim
 Set-Alias -Name j -Value z
-
-if ($host.Name -eq 'ConsoleHost'){
-    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args}
-    Set-Alias -Name ls -Value ls_git -Option Private
-}
 
 function .. {
     cd ..
